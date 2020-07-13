adb8772ac880a45b8007c02f0f396933ab2f5723
gpaul <gpaul@e3r4p10.42.fr>
2020-07-13 20:23:17 +0200

    -convert base + new conv base

--- ex04/ft_convert_base.c
+++ ex04/ft_convert_base.c
@@ -6,7 +6,7 @@
 /*   By: gpaul <gpaul@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/07/08 19:12:41 by gpaul             #+#    #+#             */
-/*   Updated: 2020/07/13 15:37:04 by gpaul            ###   ########.fr       */
+/*   Updated: 2020/07/13 17:19:33 by gpaul            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,12 +16,11 @@ int		ft_check_base(char *base);
 int		ft_atoi_base(char *str, char *base);
 int		ft_strlen(char *str);
 
-
 int		ft_nb_char(int nbr, char *base)
 {
-	unsigned int 	len_base;
-	unsigned int 	nb;
-	int				c;
+	unsigned int		len_base;
+	unsigned int		nb;
+	int					c;
 
 	c = 0;
 	nb = nbr;
@@ -42,44 +41,66 @@ int		ft_nb_char(int nbr, char *base)
 	return (c);
 }
 
+char	*ft_rev(char *re)
+{
+	int		i;
+	int		size;
+	char	temp;
 
+	size = ft_strlen(re);
+	i = 0;
+	while (i < size)
+	{
+		temp = re[i];
+		re[i] = re[size - 1];
+		re[size - 1] = temp;
+		i++;
+		size--;
+	}
+	return (re);
+}
 
-char	*ft_convert_nbr(unsigned int atoi, int nb, char *base, char *re)
+char	*ft_convert_nbr(int origin_nb, unsigned int nb, char *base_to, char *re)
 {
 	unsigned int	len_base;
 	int				i;
 
 	i = 0;
-	len_base = ft_strlen(base);
-	while (atoi > 0)
+	len_base = ft_strlen(base_to);
+	if (nb == 0)
 	{
-		re[i] = (base[atoi % len_base]);
-		atoi = atoi / len_base;
+		re[i] = base_to[0];
 		i++;
 	}
-	if (nb < 0)
+	while (nb > 0)
+	{
+		re[i] = (base_to[nb % len_base]);
+		nb = nb / len_base;
+		i++;
+	}
+	if (origin_nb < 0)
 		re[i++] = '-';
 	re[i] = '\0';
+	ft_rev(re);
 	return (re);
 }
 
 char	*ft_convert_base(char *nbr, char *base_from, char *base_to)
 {
-	int		nb;
-	char	*atoi_res;
-	char 	*res;
-	unsigned atoi;
+	int			nb;
+	char		*re;
+	int			origin_nb;
 
 	nb = ft_atoi_base(nbr, base_from);
-	atoi = nb;
+	origin_nb = nb;
 	if (nb == 0)
 		return (NULL);
-	if (!(atoi_res = malloc(sizeof(int) * ft_nb_char(nb, base_from) + 1)))
-			return(0);
-	if (!(res = malloc(sizeof(char) * 40 + 1)))
-			return (0);
-	res = ft_convert_nbr(atoi, nb, base_to, atoi_res);
-	if (res == NULL)
-		return(NULL);
-	return (res);
+	if (nb < 0)
+		nb = -nb;
+	if (ft_nb_char(nb, base_to) == 0)
+		return (NULL);
+	if (!(re = malloc(sizeof(char) * ft_nb_char(nb, base_to))))
+		return (0);
+	re = ft_convert_nbr(origin_nb, nb, base_to, re);
+	return (re);
 }
